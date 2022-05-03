de33e84a99117021b6bbd28bd18bee63781b93ee
Ilya <ilia.juck@gmail.com>
2022-05-04 00:06:19 +0300

    add round text

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,18 +1,18 @@
-function rotateMain(value) {
-    const wheelInner = document.querySelectorAll('.wheel__inner')
-    deg = 0;
-    for (let item of wheelInner) {
-        item.style.transform = `rotate(${deg}deg)`
-        deg += value;
-    }
-
-    return value
-}
-
-rotateMain(5);
+// function rotateMain(value) {
+//     const wheelInner = document.querySelectorAll('.wheel__inner')
+//     deg = 0;
+//     for (let item of wheelInner) {
+//         item.style.transform = `rotate(${deg}deg)`
+//         deg += value;
+//     }
+//
+//     return value
+// }
+//
+// rotateMain(0);
 
 function rotateOuther(value) {
-    const wheelContent = document.querySelectorAll('.wheel__content')
+    const wheelContent = document.querySelectorAll('.wheel__letter')
     deg = 0;
     for (let item of wheelContent) {
         item.style.transform = `rotate(${deg}deg)`
@@ -21,4 +21,15 @@ function rotateOuther(value) {
 }
 
 rotateOuther(10)
+
+function rotateWord(value) {
+    const wheelContent = document.querySelectorAll('.wheel__word')
+    deg = 0;
+    for (let item of wheelContent) {
+        item.style.transform = `rotate(${deg}deg)`
+        deg += value;
+    }
+}
+
+rotateWord(20)
 
