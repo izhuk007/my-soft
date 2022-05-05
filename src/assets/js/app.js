594d89568248525c02837a85b267a9f702b613ae
Ilya <ilia.juck@gmail.com>
2022-05-05 23:52:53 +0300

    add swiper

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,35 +1,67 @@
-// function rotateMain(value) {
-//     const wheelInner = document.querySelectorAll('.wheel__inner')
-//     deg = 0;
-//     for (let item of wheelInner) {
-//         item.style.transform = `rotate(${deg}deg)`
-//         deg += value;
-//     }
-//
-//     return value
-// }
-//
-// rotateMain(0);
+let pageSlider = new Swiper(".page", {
+  // Свои классы
+  wrapperClass: "page__wrapper",
+  slideClass: "page__screen",
 
-function rotateOuther(value) {
-    const wheelContent = document.querySelectorAll('.wheel__letter')
-    deg = 0;
-    for (let item of wheelContent) {
-        item.style.transform = `rotate(${deg}deg)`
-        deg += value;
-    }
-}
+  // Вертикальный слайдер
+  direction: "vertical",
 
-rotateOuther(10)
+  // Количество слайдов для показа
+  slidesPerView: "auto",
 
-function rotateWord(value) {
-    const wheelContent = document.querySelectorAll('.wheel__word')
-    deg = 0;
-    for (let item of wheelContent) {
-        item.style.transform = `rotate(${deg}deg)`
-        deg += value;
-    }
-}
+  // Управление клавиатурой
+  keyboard: {
+    // Включить\выключить
+    enabled: true,
+    // Включить\выключить
+    // только когда слайдер
+    // в пределах вьюпорта
+    onlyInViewport: true,
+    // Включить\выключить
+    // управление клавишами
+    // pageUp, pageDown
+    pageUpDown: true,
+  },
 
-rotateWord(20)
+  // Управление колесом мыши
+  mousewheel: {
+    // Чувствительность колеса мыши
+    sensitivity: 1,
+    // Класс объекта на котором
+    // будет срабатывать прокрутка мышью.
+    //eventsTarget: ".image-slider"
+  },
 
+  // Отключение функционала
+  // если слайдов меньше чем нужно
+  watchOverflow: true,
+
+  // Скорость
+  speed: 800,
+
+  // Обновить свайпер
+  // при изменении элементов слайдера
+  observer: true,
+
+  // Обновить свайпер
+  // при изменении родительских
+  // элементов слайдера
+  observeParents: true,
+
+  // Обновить свайпер
+  // при изменении дочерних
+  // элементов слайда
+  observeSlideChildren: true,
+
+  // текущее положение
+  on: {
+    slideChange: function () {
+      let currentSlide = this.realIndex + 1;
+      document.querySelector(".current-slide").innerHTML = currentSlide;
+    },
+    init: function () {
+      let currentSlide = this.realIndex + 1;
+      document.querySelector(".current-slide").innerHTML = currentSlide;
+    },
+  },
+});
